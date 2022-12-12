5151b0a8609cd56558803e8aebb7ca039f8f43bf
Galchenko-Artem <artem.galchenko.88@gmail.com>
2022-12-12 11:33:22 +0300

    CS Dota done

--- client/src/components/Dota/ListOfAccounts/ListAccDota.jsx
+++ client/src/components/Dota/ListOfAccounts/ListAccDota.jsx
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './StyleAccounts.css';
-import { useDispatch } from 'react-redux';
-import { basketAdd } from '../../../store/actions/basketAction';
+import { useDispatch, useSelector } from 'react-redux';
+import { basketAdd, basketDel } from '../../../store/actions/basketAction';
 
 export default function ListAccDota() {
   const [acc, setAcc] = useState();
+  const basket = useSelector((store) => store.basketStore);
+
   const dispatch = useDispatch();
   useEffect(() => {
     fetch('http://localhost:3001/dota2/listOfAccounts', {
@@ -22,17 +24,40 @@ export default function ListAccDota() {
   }, []);
 
   const addToBasket = (el) => {
-    dispatch(basketAdd(el));
-    // fetch('http://localhost:3001/basket', {
-    //   credentials: 'include',
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-type': 'application/json',
-    //   },
-    //   body: JSON.stringify(el),
-    // })
-    //   .then((res) => res.json())
-    //   .then((res) => console.log(res));
+    const isInBasket = basket.some((item) => item.id === el.id);
+    if (!isInBasket) {
+      dispatch(basketAdd(el));
+      console.log('Добавляем в редакс так как его нет в корзине');
+    }
+    fetch('http://localhost:3001/basket', {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(el),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
+  const removeFromBasket = (el) => {
+    dispatch(basketDel(el.id));
+    fetch('http://localhost:3001/basket', {
+      method: 'DELETE',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(
+        el,
+      ),
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .catch();
   };
 
   return (
@@ -56,7 +81,11 @@ export default function ListAccDota() {
                       </div>
                               <div>{el.price} $</div>
                                   <div>
-          <button onClick={() => addToBasket(el)} id={el.id}>Корзина</button>
+                                  {basket.some((item) => item.id === el.id) ? (
+                                    <button className="inBasket" onClick={() => removeFromBasket(el)}>В корзине</button>
+                                  ) : (
+                                    <button onClick={(e) => addToBasket(el)}>В корзину</button>
+                                  )}
                                   </div>
               </div>
 
