1024ceb4457f1e1d2ee36352930a56627e67c9ca
Vladislav <vlad.devidson.harle@gmail.com>
2022-12-13 13:41:27 +0300

    Logo

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 // import Maps from './Map/Map';
-import './styleFooter.css';
 import { Link } from 'react-router-dom';
+import styles from './styleFooter.module.css';
 
 export default function Footer() {
   return (
-    <footer className="footer">
+    <footer className={styles.footer}>
         <hr />
-        <Link to="/teleg"><img className="imgTeleg" src="https://w7.pngwing.com/pngs/12/974/png-transparent-social-telegram-chat-messenger-social-network-icon.png" alt="telega" /></Link>
+      <Link to="/teleg"><img className={styles.imgTeleg} src="https://w7.pngwing.com/pngs/12/974/png-transparent-social-telegram-chat-messenger-social-network-icon.png" alt="telega" /></Link>
         <Link to="/contacts"> Контакты </Link>
     </footer>
   );
--- client/src/components/Footer/Сontacts/Сontacts.jsx
+++ client/src/components/Footer/Сontacts/Сontacts.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Maps from './Map/Map';
+import styles from '../styleFooter.module.css';
 
 export default function Сontacts() {
   return (
-    <div className="containerCont">
-      <div className="conatainerCreator">
+  <div className={styles.containerCont}>
+      <div className={styles.conatainerCreator}>
          <h1>Создатели проекта:</h1>
       </div>
     <div>
         <Maps />
     </div>
-    </div>
+  </div>
 
   );
 }
